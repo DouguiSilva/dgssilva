3deb90e4ff50f99c05b8f1575d128a5c5dccb5f4
Douglas Silva <ikkarusdx@gmail.com>
2018-05-03 23:06:28 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,64 @@
 $(document).ready(function() {
 
-    $('input').blur(function() {
+    // Get the modal
 
-        if ($(this).val())
-            $(this).addClass('used');
-        else
-            $(this).removeClass('used');
 
-    });
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var modal = document.getElementById('myModal');
+    var img = document.getElementById('myImg');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+    // var modal = document.getElementsByClassName('modal');
+    // var img = document.getElementsByClassName('port-cell');
+    // var modalImg = document.getElementsByClassName("img");
+    // var captionText = document.getElementsByClassName("caption");
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // $('input').blur(function() {
+
+    //     if ($(this).val())
+    //         $(this).addClass('used');
+    //     else
+    //         $(this).removeClass('used');
+
+    // });
+
+
+
+    // MEGA TESTE
+    // document.getElementById("myImage").style.display = "none";
+
+    // var modal = document.getElementById('myModal');
+
+    // var img = document.getElementById('myImg');
+    // var modalImg = document.getElementById("img01").src = "css/images/i-sandshrew.png";
+
+    // var captionText = document.getElementById("caption");
+    // img.onclick = function() {
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    // }
+
+
+    // var span = document.getElementsByClassName("close")[0];
+
+    // span.onclick = function() {
+    //     modal.style.display = "none";
+    // }
 
 
 });
