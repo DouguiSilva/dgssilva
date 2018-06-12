b50849671096497a921c79df029e6bfbaf43d1b3
Douglas Silva <ikkarusdx@gmail.com>
2018-06-12 15:08:35 -0300

    Add files via upload
    Resolvido o problema com as imagens da galeria do portfólio. Agora todas as imagens estão abrindo uma modal e passando/voltando para a imagem seguinte/posterior. Falta só trocar as imagem para o tamanho grande.

--- js/main.js
+++ js/main.js
@@ -4,57 +4,23 @@ $(document).ready(function() {
 
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var modal = document.getElementById('myModal');
-    var img = document.getElementById('myImg');
-    var modalImg = document.getElementById("img01").src = "css/images/i-sandshrew.png";
-    var captionText = document.getElementById("caption");
-
-    img.onclick = function() {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // $('input').blur(function() {
-
-    //     if ($(this).val())
-    //         $(this).addClass('used');
-    //     else
-    //         $(this).removeClass('used');
-
-    // });
-
-
-
-    // MEGA TESTE
-    // document.getElementById("myImage").style.display = "none";
-
     // var modal = document.getElementById('myModal');
-
     // var img = document.getElementById('myImg');
     // var modalImg = document.getElementById("img01").src = "css/images/i-sandshrew.png";
-
     // var captionText = document.getElementById("caption");
+
     // img.onclick = function() {
     //     modal.style.display = "block";
     //     modalImg.src = this.src;
     //     captionText.innerHTML = this.alt;
     // }
 
-
+    // Get the <span> element that closes the modal
     // var span = document.getElementsByClassName("close")[0];
 
-    // span.onclick = function() {
-    //     modal.style.display = "none";
-    // }
-
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 
 });
